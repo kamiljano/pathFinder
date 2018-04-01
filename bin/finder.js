60b561be3a07c29b3564c11465e5ea02afcd1276
Kamil Janowski <kamil.janowski@f-secure.com>
2018-04-01 17:52:41 +0300

    send sms to janoka

--- bin/finder.js
+++ bin/finder.js
@@ -1,11 +1,22 @@
 'use strict';
 
 const request = require('request-promise');
+const AWS = require('aws-sdk');
 
 const PORT_PROTOCOL_MATRIX = Object.freeze({
   http: [80, 8080, 9080],
   https: [443, 9443]
 });
+
+const publish = url => {
+  const sns = new AWS.SNS({region: 'eu-west-1'});
+  const params = {
+    Message: 'Found VCS at ' + url,
+    MessageStructure: 'string',
+    PhoneNumber: '+358406673825'
+  };
+  return sns.publish(params).promise();
+};
 
 module.exports.findRepositories = address => {
   return Promise.all(Object.entries(PORT_PROTOCOL_MATRIX).map(([protocol, ports]) => {
@@ -14,6 +25,7 @@ module.exports.findRepositories = address => {
       try {
         await request.get(url);
         console.log(`Positive result at ${url}.`);
+        await publish(url);
       } catch (err) {
         console.debug(`Nothing found for ${url}`);
       }
