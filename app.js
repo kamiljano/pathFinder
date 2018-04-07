13e406bf9527e0c43e847f7b6fe3bd61821fe879
Kamil Janowski <kamil.janowski@f-secure.com>
2018-04-07 13:09:22 +0300

    report saving

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const ipterate = require('ipterate');
 const PathFinder = require('./bin/finder').PathFinder;
+const reportingService = require('./bin/reportingService');
 const commandLineArgs = require('command-line-args');
 
 const options = commandLineArgs([
@@ -10,6 +11,9 @@ const options = commandLineArgs([
 
 const pathFinder = new PathFinder(options.path);
 
-ipterate.range('0.0.0.0/0').iterateAsync(ip => {
-  return pathFinder.find(ip);
+ipterate.range('0.0.0.0/0').iterateAsync(async (ip, data) => {
+  const result = await pathFinder.find(ip);
+  if (data.iteration % 256 === 0) {
+    reportingService.saveMilestone(ip, options.path);
+  }
 });
--- bin/finder.js
+++ bin/finder.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const request = require('request-promise');
+let request = require('request-promise');
+const urlUtil = require('url');
+let reportingService = require('./reportingService');
 
 const PORT_PROTOCOL_MATRIX = Object.freeze({
   http: [80, 8080, 9080],
@@ -13,12 +15,13 @@ class PathFinder {
   }
 
   find(ipAddress) {
-    return Promise.all(Object.entries(PORT_PROTOCOL_MATRIX).map(([protocol, ports]) => {
+    Promise.all(Object.entries(PORT_PROTOCOL_MATRIX).map(([protocol, ports]) => {
       return Promise.all(ports.map(async port => {
-        const url = `${protocol}://${ipAddress}:${port}${this.path}`;
+        const url = urlUtil.resolve(`${protocol}://${ipAddress}:${port}`, this.path);
         try {
           await request.get(url);
           console.log(`Positive result at ${url}.`);
+          reportingService.updateReport(this.path, url);
         } catch (err) {
           console.debug(`Nothing found for ${url}`);
         }
